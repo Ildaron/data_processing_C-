16f603c1d1767ab0adf5dcd9702211ab49c6d97d
Ildar <de.ikar@yandex.ru>
2020-07-15 13:20:43 +0900

    Update 1.Serial_send.cpp

--- 1.Serial_send.cpp
+++ 1.Serial_send.cpp
@@ -1,10 +1,10 @@
 #include<windows.h>
 #include<stdio.h>
 int main()
-{
+{ 
   HANDLE hComm;
 
-  hComm = CreateFile("COM24",                //port name
+  hComm = CreateFile("\\\\.\\COM18",                //port name
                       GENERIC_READ | GENERIC_WRITE, //Read/Write
                       0,                            // No Sharing
                       NULL,                         // No Security
@@ -12,10 +12,12 @@ int main()
                       0,            // Non Overlapped I/O
                       NULL);        // Null for Comm Devices
 
-  if (hComm == INVALID_HANDLE_VALUE)
-	  int a=34;
+  if (hComm == INVALID_HANDLE_VALUE)//{
+//	  int a=34;
      //std::cout << "Hello World!\n";
-  //else
+      printf("not connect\n");//}
+  else//{
+	  printf("connected\n");//}
     //  printf(“opening serial port successful”);
      //std::cout << "Hello World!\n";
   CloseHandle(hComm);//Closing the Serial Port
